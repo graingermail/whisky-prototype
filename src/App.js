9e9646f738c57a892e0d95ac03bbae27a37f2e60
Brian Grainger <graingermail@googlemail.com>
2023-10-08 16:12:15 +0100

    flavours

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Data from "./Data";
+import Flavours from "./Flavours";
 
 function App() {
   const [whiskies, setwhiskies] = useState(Data);
@@ -12,9 +13,51 @@ function App() {
     setwhiskies(filteredNames);
   };
 
+  //state for flavours
+  const [flavours, setFlavours] = useState([]);
+
+  //flavours check boxes
+  const [checkedState, setCheckedState] = useState(
+    new Array(Flavours.length).fill(false)
+  );
+
+  const handleOnChange = (position) => {
+    //tick the box
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+  };
+
   return (
     <>
+      <div>
+        <h3>Select Flavours</h3>
+        <ul>
+          {Flavours.map(({ name, id }, index) => {
+            return (
+              <li key={index}>
+                <div>
+                  <div>
+                    <input
+                      type="checkbox"
+                      id={`custom-checkbox-${index}`}
+                      name={name}
+                      value={id}
+                      checked={checkedState[index]}
+                      onChange={() => handleOnChange(index)}
+                    />
+                    {name}
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
       <h2>Whisky</h2>
+
       <input type="text" onChange={(e) => filterNames(e)} />
       <button>Search</button>
       <ul>
---
+++ src/Flavours.js
@@ -0,0 +1,14 @@
+const Flavours = [
+  { name: "Floral", id: 128 },
+  { name: "Fruity", id: 129 },
+  { name: "Vanilla", id: 130 },
+  { name: "Honey", id: 131 },
+  { name: "Spicy", id: 132 },
+  { name: "Peated", id: 133 },
+  { name: "Salty", id: 134 },
+  { name: "Woody", id: 135 },
+  { name: "Nutty", id: 136 },
+  { name: "Chocoltey", id: 137 },
+];
+
+export default Flavours;
